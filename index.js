a01e054bd80346e606a9bad3bb1a22d0336f09db
Kathund <Jacobbray2007@gmail.com>
2023-05-03 16:36:00 +0800

    Fixed issue with dungeons cyber bully

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ register('chat', (event) => {
 register('chat', (event) => {
   var message = helperFunction.removeColors((ChatLib.getChatMessage(event)).toString());
   if (!config.dungeonsCyberBully) return;
-  if (message.startsWith("PUZZLE FAIL!") && message.includes("Yikes!") || messaged.includes("[STATUE] Oruo the Omniscient") && message.includes("chose the wrong answer!")) {
+  if (message.startsWith("PUZZLE FAIL!") && message.includes("Yikes!") || message.includes("[STATUE] Oruo the Omniscient") && message.includes("chose the wrong answer!")) {
     ChatLib.command(`pc ${message}`);
     ChatLib.command(`ac ${message}`);
   }
