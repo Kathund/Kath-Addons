0d05b803d3995cd2b17590f91424caefbcbb065d
Kath <55346310+Kathund@users.noreply.github.com>
2023-05-08 08:45:27 +0800

    Removed Auto Dungeon Restart due to ban chnaces

--- index.js
+++ index.js
@@ -198,29 +198,3 @@ register("step", () => {
   if (!config.dungeonsSpiritBearESP && !config.dungeonsSpiritBowESP) return;
   new Thread(() => bows = World.getAllEntities().filter(e => e.getName().includes("Spirit"))).start();
 }).setFps(2)
-
-register('chat', (event) => {
-  var message = helperFunction.removeColors((ChatLib.getChatMessage(event)).toString());
-  if (!config.dungeonsAutoRestart) return;
-  if (message.includes("Catacombs Experience")) {
-    new Thread(() => {
-      Thread.sleep(config.dungeonsAutoRestartDelay)
-      let scoreboardInfoData = Scoreboard.getLines()
-      let scoreboardInfo = scoreboardInfoData.join()
-      if (scoreboardInfo.includes("(F1)")) ChatLib.command(`joindungeon catacombs 1`)
-      if (scoreboardInfo.includes("(F2)")) ChatLib.command(`joindungeon catacombs 2`)
-      if (scoreboardInfo.includes("(F3)")) ChatLib.command(`joindungeon catacombs 3`)
-      if (scoreboardInfo.includes("(F4)")) ChatLib.command(`joindungeon catacombs 4`)
-      if (scoreboardInfo.includes("(F5)")) ChatLib.command(`joindungeon catacombs 5`)
-      if (scoreboardInfo.includes("(F6)")) ChatLib.command(`joindungeon catacombs 6`)
-      if (scoreboardInfo.includes("(F7)")) ChatLib.command(`joindungeon catacombs 7`)
-      if (scoreboardInfo.includes("(M1)")) ChatLib.command(`joindungeon MASTER_CATACOMBS 1`)
-      if (scoreboardInfo.includes("(M2)")) ChatLib.command(`joindungeon MASTER_CATACOMBS 2`)
-      if (scoreboardInfo.includes("(M3)")) ChatLib.command(`joindungeon MASTER_CATACOMBS 3`)
-      if (scoreboardInfo.includes("(M4)")) ChatLib.command(`joindungeon MASTER_CATACOMBS 4`)
-      if (scoreboardInfo.includes("(M5)")) ChatLib.command(`joindungeon MASTER_CATACOMBS 5`)
-      if (scoreboardInfo.includes("(M6)")) ChatLib.command(`joindungeon MASTER_CATACOMBS 6`)
-      if (scoreboardInfo.includes("(M7)")) ChatLib.command(`joindungeon MASTER_CATACOMBS 7`)
-    }).start()
-  }
-})
