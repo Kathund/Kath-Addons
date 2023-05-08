eded5fa90c7877284d4f14a47ff4a5635db31882
Kath <55346310+Kathund@users.noreply.github.com>
2023-05-08 11:56:41 +0800

    Added imports so the code works

--- index.js
+++ index.js
@@ -10,3 +10,36 @@ register("chat", function (event) {
     cancel(event)
   }
 })
+
+// ! Dungeons
+
+import './feture/dungeons/bloodReady.js'
+import './feture/dungeons/cyberBullyDungeons.js'
+import './feture/dungeons/spiritBow.js';
+
+// ! Gardens
+
+import './feture/gardens/visitorAlert.js';
+
+// ! Party
+
+import './feture/party/autoPartyAccept.js';
+import './feture/party/fragBot.js';
+
+// ! Chat
+
+import './feture/chat/antiCombo.js';
+import './feture/chat/antiGexp.js';
+import './feture/chat/antiHype.js';
+import './feture/chat/antiMVPJoin.js';
+import './feture/chat/antiNon.js';
+import './feture/chat/antiSBE.js';
+import './feture/chat/antiTip.js';
+import './feture/chat/coopChatPing.js';
+import './feture/chat/cyberBully.js';
+import './feture/chat/wikiSwapper.js';
+
+// ! Misc
+
+import './feture/misc/arrowWarning.js';
+import './feture/misc/giftWaypoints.js';
