8082bc9f28a15ea3299eacc0ba9956b6bdfba7e8
Kath <55346310+Kathund@users.noreply.github.com>
2023-05-09 14:37:57 +0800

    Added Changelog in gui

--- feture/changeLog.js
+++ feture/changeLog.js
@@ -4,6 +4,8 @@ const config = Settings
 
 register("step", () => {
   if (!config.generalShowChangeLog) return;
-  if (helperFunction.data.updateMessage120) return;
-  helperFunction.showUpdateMessage();
+  if (helperFunction.data.updateMessage1_2_0) return;
+  helperFunction.data.updateMessage1_2_0 = true
+  helperFunction.data.save();
+  helperFunction.viewChangeLog1_2_0();
 }).setFps(5)
--- settings.js
+++ settings.js
@@ -1,3 +1,4 @@
+import * as helperFunctions from "./helperFunctions.js";
 let currentVersion = JSON.parse(FileLib.read("kathAddons", "metadata.json")).version
 import { @Vigilant, @ButtonProperty, @SwitchProperty, @SliderProperty } from 'Vigilance';
 @Vigilant('KathAddons', 'Kath Addons', {
@@ -6,9 +7,9 @@ import { @Vigilant, @ButtonProperty, @SwitchProperty, @SliderProperty } from 'Vi
     return categories.indexOf(a.name) - categories.indexOf(b.name);
   },
   getSubcategoryComparator: () => (a, b) => {
-    const subcategories = ["General", "Info", "Other Projects", "Credits"];
-    return subcategories.indexOf(a.getValue()[0].attributesExt.subcategory) -
-      subcategories.indexOf(b.getValue()[0].attributesExt.subcategory);
+    const subcategorys = ["General", "Info", "Other Projects", "Credits", "Change Log"];
+    return subcategorys.indexOf(a.getValue()[0].attributesExt.subcategory) -
+      subcategorys.indexOf(b.getValue()[0].attributesExt.subcategory);
   },
 })
 
@@ -216,6 +217,52 @@ class Settings {
   })
   otherProjectsAutoWelcomeBack() {
     ChatLib.command(`ct import AutoWelcomeBack`);
+  };
+
+  // ? Change Logs
+
+  @ButtonProperty({
+    name: "v1.2.0",
+    description: "View the change log for v1.2.0",
+    captureEvents: "Extra Info",
+    subcategory: "Change Log",
+    placeholder: "Show In Chat",
+  })
+  changeLog1_2_0() {
+    helperFunctions.viewChangeLog1_2_0();
+  };
+
+  @ButtonProperty({
+    name: "v1.1.2",
+    description: "View the change log for v1.1.2",
+    captureEvents: "Extra Info",
+    subcategory: "Change Log",
+    placeholder: "Show In Chat",
+  })
+  changeLog1_2_0() {
+    helperFunctions.viewChangeLog1_1_2();
+  };
+
+  @ButtonProperty({
+    name: "v1.1.1",
+    description: "View the change log for v1.1.1",
+    captureEvents: "Extra Info",
+    subcategory: "Change Log",
+    placeholder: "Show In Chat",
+  })
+  changeLog1_2_0() {
+    helperFunctions.viewChangeLog1_1_1();
+  };
+
+  @ButtonProperty({
+    name: "v1.1.0",
+    description: "View the change log for v1.1.0",
+    captureEvents: "Extra Info",
+    subcategory: "Change Log",
+    placeholder: "Show In Chat",
+  })
+  changeLog1_2_0() {
+    helperFunctions.viewChangeLog1_1_0();
   };
 
   constructor() {
