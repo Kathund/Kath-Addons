f0b5d632d971f30d1e1315045bb482165e8e4328
Kath <55346310+Kathund@users.noreply.github.com>
2023-05-10 14:33:59 +0800

    1.1.3

--- helperFunctions.js
+++ helperFunctions.js
@@ -3,24 +3,3 @@ export const removeColors = function removeColors(text) {
   const cleanText = text.replace(/§./g, "");
   return cleanText;
 }
-export const divider = "&a&m            &d&m            &e&m            &a&m            &d&m            &e&m            &a&m           &d&m           &e&m           &8\n";
-
-export const showChangeMessage = () => {
-
-  let stringArray = [
-    `${divider}`,
-    ChatLib.getCenteredText(`&dKath Addons has moved to kath`) + "\n",
-    ChatLib.getCenteredText(`&d/ct delete KathAddons`) + "\n",
-    ChatLib.getCenteredText(`&d/ct import Kath`) + "\n",
-    ChatLib.getCenteredText(`&dThis message wont go away lmaooo`) + "\n",
-    `\n${divider}`
-  ]
-
-  let finalText = ""
-  stringArray.forEach((line) => {
-    if (line == "-/*") return
-    finalText += (!line.includes("-/-")) ? `\n${line}` : line.replace("-/-", "\n")
-  })
-
-  ChatLib.chat(finalText)
-}
